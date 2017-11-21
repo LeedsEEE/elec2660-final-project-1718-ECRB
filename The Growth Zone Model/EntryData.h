181a5ceff60dd35c00b04058b4542acc141bd61e
Edward Baker [el16ecrb] <el16ecrb@eentpc95.leeds.ac.uk>
2017-11-21 15:43:40 +0000

    Create Model Classes

---
+++ The Growth Zone Model/EntryData.h
@@ -0,0 +1,13 @@
+//
+//  EntryData.h
+//  The Growth Zone Model
+//
+//  Created by Edward Baker [el16ecrb] on 21/11/2017.
+//  Copyright © 2017 Edward Baker [el16ecrb]. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface EntryData : NSObject
+
+@end
---
+++ The Growth Zone Model/SubjectData.h
@@ -0,0 +1,13 @@
+//
+//  SubjectData.h
+//  The Growth Zone Model
+//
+//  Created by Edward Baker [el16ecrb] on 21/11/2017.
+//  Copyright © 2017 Edward Baker [el16ecrb]. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SubjectData : NSObject
+
+@end
