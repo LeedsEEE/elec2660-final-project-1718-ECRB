1f61ff22167a72236d15cf6d902fcd503a5d331c
Edward Baker <edwardbaker@Edwards-Mac.local>
2017-11-29 02:15:56 +0000

    Moved Files

---
+++ The Growth Zone Model/EntryTableViewController.m
@@ -0,0 +1,134 @@
+//
+//  EntryTableViewController.m
+//  The Growth Zone Model
+//
+//  Created by Edward Baker on 24/11/2017.
+//  Copyright © 2017 Edward Baker [el16ecrb]. All rights reserved.
+//
+
+#import "EntryTableViewController.h"
+
+@interface EntryTableViewController ()
+
+@end
+
+@implementation EntryTableViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    self.data = [[DataModel alloc] init]; //Initialise DataModel
+    self.date = [[Date alloc] init];    //Initialise local DateFormatter
+    
+    NSLog(@"%@",self.subjectID);
+    
+    if (self.subjectID) {
+        self.subject = self.data.subjects[self.subjectID];
+    } else {
+        NSLog(@"No subjectID");
+    }
+    
+    // Uncomment the following line to preserve selection between presentations.
+    // self.clearsSelectionOnViewWillAppear = NO;
+    
+    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (void)viewWillAppear:(BOOL)animated {
+    self.data.subjects = [self.data load];
+    [super viewWillAppear:animated];
+    [self.tableView reloadData];
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    if (self.subject) {
+        return [self.data.subjects[self.subjectID][@"entrys"] allKeys].count;
+    } else {
+        return 0;
+    }
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"EntryCell" forIndexPath:indexPath];
+    
+    
+    NSArray *getkeys = [self.data.subjects[self.subjectID][@"entrys"] allKeys];
+    NSString *entryID = [getkeys objectAtIndex:indexPath.row];
+    
+    cell.textLabel.text = entryID;
+    
+    return cell;
+}
+
+
+/*
+// Override to support conditional editing of the table view.
+- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
+    // Return NO if you do not want the specified item to be editable.
+    return YES;
+}
+*/
+
+/*
+// Override to support editing the table view.
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
+    if (editingStyle == UITableViewCellEditingStyleDelete) {
+        // Delete the row from the data source
+        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
+    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
+        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+    }   
+}
+*/
+
+/*
+// Override to support rearranging the table view.
+- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath {
+}
+*/
+
+/*
+// Override to support conditional rearranging of the table view.
+- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath {
+    // Return NO if you do not want the item to be re-orderable.
+    return YES;
+}
+*/
+
+
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+    if ([[segue identifier] isEqualToString:@"showEntryDetail"]) {
+        
+        EntryViewController *destinationViewController = [segue destinationViewController];
+        NSIndexPath *indexPath = [self.tableView indexPathForSelectedRow];
+        
+        NSArray *getkeys = [self.data.subjects[self.subjectID][@"entrys"] allKeys];
+        NSString *entryID = [getkeys objectAtIndex:indexPath.row];
+        
+        destinationViewController.entryID = entryID;
+        destinationViewController.subjectID = self.subjectID;
+        
+        self.splitViewController.preferredDisplayMode = UISplitViewControllerDisplayModePrimaryHidden; //When you select an entry the master view is hidden
+        
+    }
+}
+
+
+@end
