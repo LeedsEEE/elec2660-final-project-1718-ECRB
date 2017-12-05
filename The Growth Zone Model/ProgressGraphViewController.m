fae0b2eda37ecb979067fb6301301d97c0f54a71
Edward Baker <edwardbaker@Edwards-Mac.local>
2017-12-05 21:57:38 +0000

    Started creating progress graph

--- The Growth Zone Model/ProgressGraphViewController.m
+++ The Growth Zone Model/ProgressGraphViewController.m
@@ -19,37 +19,9 @@
     
     self.graphArray = [self dataArray];
     
-    //[self.view addSubview:[self groundedTrapezium:[UIColor grayColor] posx:30 width:40 height1:10 height2:20]];
-
-    NSLog(@"%@",self.subjectID);
+    
+    
     // Do any additional setup after loading the view.
-}
-
-- (void) drawRect:(CGRect)rect {
-    CGContextRef context = UIGraphicsGetCurrentContext();
-    
-    int width = 40;
-    int height = 40;
-    
-    
-    CGContextSetFillColorWithColor(context, [UIColor grayColor].CGColor);
-    
-    CGContextBeginPath(context);
-    CGContextMoveToPoint(context, width / 4, height / 4);
-    CGContextAddLineToPoint(context, width * 3 / 4, height / 2);
-    CGContextAddLineToPoint(context, width / 4, height * 3 / 4);
-    CGContextClosePath(context);
-    
-    CGContextSetRGBFillColor(context, 1, 1, 0, 1);
-    CGContextFillPath(context);
-    
-    /*
-    UIView *trapezium = [[UIView alloc] init];
-    [trapezium.layer renderInContext:context];
-    trapezium.backgroundColor = [UIColor grayColor];
-    trapezium.layer.masksToBounds = YES;
-    */
-    
 }
 
 -(NSArray *)dataArray
@@ -70,46 +42,16 @@
             
             [tempDataArray addObject:singleEntryArray];
         }
-        return tempDataArray;
     } else {
-        return nil;
+        NSMutableArray *tempDataArray = [[NSMutableArray alloc] init];
+        
+        [tempDataArray addObject: [[NSNumber alloc] initWithFloat:0.25]];
+        [tempDataArray addObject: [[NSNumber alloc] initWithFloat:0.25]];
+        [tempDataArray addObject: [[NSNumber alloc] initWithFloat:0.25]];
     }
+    return tempDataArray;
 }
 
-- (UIView *)groundedTrapezium:(UIColor *)colour posx:(int)posx width:(int)width height1:(int)height1 height2:(int)height{
-    
-    CGContextRef context = UIGraphicsGetCurrentContext();
-    
-    
-    
-    CGContextSetFillColorWithColor(context, colour.CGColor);
-    
-    CGContextBeginPath(context);
-    CGContextMoveToPoint(context, width / 4, height1 / 4);
-    CGContextAddLineToPoint(context, width * 3 / 4, height1 / 2);
-    CGContextAddLineToPoint(context, width / 4, height1 * 3 / 4);
-    CGContextAddLineToPoint(context, width / 4, height1 / 4);
-    CGContextFillPath(context);
-    
-    /*
-    UIView *trapezium = [[UIView alloc] init];
-    [trapezium.layer renderInContext:context];
-    trapezium.backgroundColor = colour;
-    trapezium.layer.masksToBounds = YES;
-    
-    return trapezium;
-    */
-    return NULL;
-}
-
-
-- (UIView *)circleWithColor:(UIColor *)color radius:(int)radius posx:(int)posx posy:(int)posy {
-    UIView *circle = [[UIView alloc] initWithFrame:CGRectMake(posx, posy, radius*2, radius*2)];
-    circle.backgroundColor = color;
-    circle.layer.cornerRadius = radius;
-    circle.layer.masksToBounds = YES;
-    return circle;
-}
 
 - (UIView *)rectWithColor:(UIColor *)color posx:(int)posx posy:(int)posy width:(int)width height:(int)height{
     UIView *circle = [[UIView alloc] initWithFrame:CGRectMake(posx, posy, width, height)];
