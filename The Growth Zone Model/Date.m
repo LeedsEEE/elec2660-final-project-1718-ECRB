db3f681edee7ccb92deb5f567fc7edb42a91a109
Edward Baker <edwardbaker@Edwards-Mac.local>
2017-12-08 23:24:20 +0000

    Formatting Date

--- The Growth Zone Model/Date.m
+++ The Growth Zone Model/Date.m
@@ -21,7 +21,6 @@
         
         self.dateFormatter.locale = [[NSLocale alloc] initWithLocaleIdentifier:@"en_GB"]; //Use the english date format
         [self.dateFormatter setLocalizedDateFormatFromTemplate:@"yMd"];                   //Set the date to day month year
-//        [self.dateFormatter setDateFormat:@"dd"];                                         //Give the date as a string
         
     }
     return self;
